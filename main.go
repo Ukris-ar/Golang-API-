f13ac30a8d1054a67ae99bab7d4d61486a94902a
Ukris.ar <ukris.ar@gmail.com>
2019-09-06 23:43:32 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main 
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type CityStruct struct {
+	ID				string
+	Name			string
+	CountryCode		string
+	District		string
+	Population		string
+}
+
+// Init City struct
+var city []CityStruct
+
+func dbConn() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := "root"
+    dbPass := ""
+    dbName := "World"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	
+    if err != nil {
+        panic(err.Error())
+	}
+	
+    return db
+}
+
+func getCountryAll(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	result, err := db.Query("SELECT * FROM city LIMIT 1000;")
+
+	if err != nil {
+        panic(err.Error())
+	}
+	
+	fetchData := CityStruct{}
+	for result.Next() {
+        err = result.Scan(&fetchData.ID,&fetchData.Name,&fetchData.CountryCode,&fetchData.District,&fetchData.Population)
+        if err != nil {
+            panic(err.Error())
+		}
+
+		// Assign to Struct
+		city = append(city, fetchData);
+	}
+
+	json.NewEncoder(w).Encode(city)
+ }
+
+func homePage(w http.ResponseWriter, r *http.Request) { 
+    fmt.Fprint(w, "Welcome to the HomePage!")
+}
+func handleRequest() {
+	http.HandleFunc("/", homePage) 
+	http.HandleFunc("/getCountry", getCountryAll) 
+    http.ListenAndServe(":8080", nil) 
+}
+
+func main() {
+	handleRequest()
+}
